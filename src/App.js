66db682353108a6bdc4f8d65c3548c4db31f0dc2
Zoom <egor147536987@gmail.com>
2024-01-28 18:29:45 +0300

    Reorganize the codebase and UI. (generated by blackbox.ai )

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
-import styled from 'styled-components'
+import React, { useState, useEffect } from "react";
+import './App.css'
+import styled from 'styled-components';
+
+
 
 const Button = styled.button`
   background: transparent;
@@ -13,18 +16,37 @@ const Button = styled.button`
   width: 45%;
 `
 
-const Block = styled.button`
+const AppContainer = styled.div`
+  margin: auto 2vw;
+`
+const Image = styled.img`
+  max-width:50%;
+  height:auto;
+  background: url(${props => props.src});ckground: url(${props => props.src});
+`
+
+const InfContainer = styled.div`
+  width:50%
+`
+
+
+const Block = styled.div`
+  display: flex;  
   background: transparent;
-  border-radius: 3px;
+  border-radius: 15px;
   border: 2px solid #BF4F74;
   color: #BF4F74;
-  width: 45%;
+  width: 100%;
+  align-items:center;
+  text-align:center;
   cursor:pointer;
+  overflow: hidden;
 `
 const Container = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: space-between;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(40%, 1fr));
+  grid-gap: 32px;
+  grid-auto-flow: dense;
   align-items: center;
   max-width:100vw;
 `
@@ -60,23 +82,22 @@ export default function App() {
     getData(num);
   }, [num]);
   return (
-    <div className="App">
+    <AppContainer>
       <h1>API Posts</h1>
       {loading && <div>A moment please...</div>}
       <Container>
         {data &&
-          data.map(({ id, name }) => (
-            <Block key={id}>
-              <h3>{id} {name}</h3>
+          data.map((data) => (
+            <Block key={data.id}>
+              <Image style={{ float: "left", }} src={data.image} alt="Profile image" />
+              <InfContainer>
+                <h3>{data.name}</h3>
+                <p>{data.gender} - {data.status}</p>
+              </InfContainer>
             </Block>
           ))}
       </Container>
       <ButtonContainer>
-        <Button onClick={() => {
-          setNum(num => num + 1)
-        }}>
-          Load More
-        </Button>
         <Button onClick={() => {
           num <= 1
             ? setNum(1)
@@ -84,8 +105,12 @@ export default function App() {
         }}>
           Back
         </Button>
+        <Button onClick={() => {
+          setNum(num => num + 1)
+        }}>
+          Load More
+        </Button>
       </ButtonContainer>
-
-    </div>
+    </AppContainer>
   );
 }
--- src/components/TheFooter.tsx
+++
@@ -1,5 +0,0 @@
-const TheFooter = () => {
-  return <footer>Created by &copy;<a href="https://github.com/Zoomish">Zoomish</a></footer>;
-};
-
-export { TheFooter };
